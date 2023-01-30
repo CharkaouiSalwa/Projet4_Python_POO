716fe9bb749b877b8d9510dd95e2db6aad179a68
CharkaouiSalwa <salwacharkaoui@gmail.com>
2023-01-30 20:05:01 +0100

    add sous_menu in class main

--- main.py
+++ main.py
@@ -8,36 +8,49 @@ try:
             4: 'Gestion des matchs',
             5: 'Quitter'
         }
+        sous_tournois = {
+            1: 'Ajouter un tournoi',
+            2: 'Afficher tous les tournois',
+            3: 'Afficher nom et date du tournois',
+            4: 'Retour au menu principal',
+            5: 'Quitter'
+        }
+        sous_tours = {
+            1: 'Ajouter un tour',
+            2: 'Fermer un tour',
+            3: 'Afficher les tours du tournois',
+            4: 'Retour au menu principal',
+            5: 'Quitter'
 
-        def print_menu():
+        }
+        sous_joueurs = {
+            1: 'Ajouter un joueur',
+            2: 'Afficher tous les joueurs',
+            3: 'Afficher les joueurs du tournois',
+            4: 'Retour au menu principal',
+            5: 'Quitter'
+        }
+        sous_match = {
+            1: 'Definir un gagnant',
+            2: 'Afficher les matchs d un tour',
+            3: 'Retour au menu principal',
+            4: 'Quitter'
+        }
+
+        def print_menu(menu):
             for key in menu_options.keys():
-                print(key, '--', menu_options[key])
-
-        def gestion_tournois():
-             sous_tournois  = {
-
-             }
-
-        def gestion_tours():
-             print('choisir l option \'Gestion des tours\'')
-
-        def gestion_joueurs():
-             print('choisir l option \'Gestion des joueurs\'')
-
-        def gestion_match():
-            print('choisir l option \'Gestion des matchs\'')
-
-        if __name__=='__main__':
-            while(True):
-                print_menu()
+                print(key, '--', menu[key])
+        def menu_principal():
+            while (True):
+                print_menu(menu_options)
                 option = ''
                 try:
                     option = int(input('Entrer voitre choix: '))
                 except:
                     print('Erreur. Entrer un numero ...')
-                #Check what choice was entered and act accordingly
+                # Check what choice was entered and act accordingly
                 if option == 1:
-                   gestion_tournois()
+                    gestion_tournois()
                 elif option == 2:
                     gestion_tours()
                 elif option == 3:
@@ -49,5 +62,98 @@ try:
                     exit()
                 else:
                     print('Option ivalide. Svp entrer un numero entre 1 et 5.')
-except Exception as e:
+
+        def gestion_tournois():
+            while (True):
+                print_menu(sous_tournois)
+                option = ''
+                try:
+                    option = int(input('Entrer voitre choix: '))
+                except:
+                    print('Erreur. Entrer un numero ...')
+                # Check what choice was entered and act accordingly
+                if option == 1:
+                   print('ajouter un tournoi')
+                elif option == 2:
+                    print('Afficher tous les tournois')
+                elif option == 3:
+                    print('Afficher nom et date du tournois')
+                elif option == 4:
+                   menu_principal()
+                elif option == 5:
+                    print('Merci pour votre visite, à bientot !')
+                    exit()
+                else:
+                    print('Option ivalide. Svp entrer un numero entre 1 et 5.')
+
+        def gestion_tours():
+            while (True):
+                print_menu(sous_tours)
+                option = ''
+                try:
+                    option = int(input('Entrer voitre choix: '))
+                except:
+                    print('Erreur. Entrer un numero ...')
+                # Check what choice was entered and act accordingly
+                if option == 1:
+                    print('ajouter un tour')
+                elif option == 2:
+                    print('Fermer un tour ')
+                elif option == 3:
+                    print('Afficher les tours du tournois')
+                elif option == 4:
+                    menu_principal()
+                elif option == 5:
+                    print('Merci pour votre visite, à bientot !')
+                    exit()
+                else:
+                    print('Option ivalide. Svp entrer un numero entre 1 et 5.')
+
+        def gestion_joueurs():
+            while (True):
+                print_menu(sous_joueurs)
+                option = ''
+                try:
+                    option = int(input('Entrer voitre choix: '))
+                except:
+                    print('Erreur. Entrer un numero ...')
+                # Check what choice was entered and act accordingly
+                if option == 1:
+                    print('ajouter un joueur')
+                elif option == 2:
+                    print('Afficher tous les joueurs')
+                elif option == 3:
+                    print('Afficher les joueurs du tournois')
+                elif option == 4:
+                    menu_principal()
+                elif option == 5:
+                    print('Merci pour votre visite, à bientot !')
+                    exit()
+                else:
+                    print('Option ivalide. Svp entrer un numero entre 1 et 5.')
+
+        def gestion_match():
+            while (True):
+                print_menu(sous_match)
+                option = ''
+                try:
+                    option = int(input('Entrer voitre choix: '))
+                except:
+                    print('Erreur. Entrer un numero ...')
+                # Check what choice was entered and act accordingly
+                if option == 1:
+                    print('definir un gagnant')
+                elif option == 2:
+                    print('Afficher les matchs d un tour')
+                elif option == 3:
+                    menu_principal()
+                elif option == 4:
+                    print('Merci pour votre visite, à bientot !')
+                    exit()
+                else:
+                    print('Option ivalide. Svp entrer un numero entre 1 et 4.')
+        if __name__=='__main__':
+            menu_principal()
+
+except Exception as e:1
   print(e)
